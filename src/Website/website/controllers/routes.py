144e01d586793277ca9453712371a6ac1bd13400
diegofariasm <diegofarias016@gmail.com>
2022-12-12 17:52:38 -0300

    begin opencv tests

--- src/Website/website/controllers/routes.py
+++ src/Website/website/controllers/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Response
 from flask_login import login_required, current_user
+from Recognition.streaming import gen
 
 routes = Blueprint("routes", __name__)
 
@@ -9,7 +10,13 @@ routes = Blueprint("routes", __name__)
 def home():
     return render_template("index.html", user=current_user)
 
+
 @routes.route("/attendance")
 @login_required
 def attendance():
     return render_template("attendance.html", user=current_user)
+
+
+@routes.route("/video-feed")
+def video_feed():
+    return Response(gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
